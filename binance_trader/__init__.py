cab525788634db9c16ce24fc076f71cd16917c96
eronbfr <eronbfr@gmail.com>
2026-04-10 08:19:53 -0300

    Delete binance_trader directory

--- binance_trader/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Binance Trading Bot - Bitcoin Trader
-Algoritmo de trading automatizado para Bitcoin na Binance.
-Visa lucro mínimo de R$1.000,00 por trade com stop-loss dinâmico.
-"""
-
-__version__ = "1.0.0"
